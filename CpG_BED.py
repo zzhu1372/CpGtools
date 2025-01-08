c486261f3b7750ecb329b46f1ab140ba37f48fd1
zzhu1372 <142303647+zzhu1372@users.noreply.github.com>
2025-01-08 16:56:26 +0100

    mean CpG methylation levels for regions in a BED file

---
+++ CpG_BED.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+from pybedtools import BedTool
+from datatable import dt, f, by, g, join, sort, update, ifelse
+
+def find_overlapping_regions(bed1_df, bed2_df, bed1_cols, bed2_cols):
+    bed_1 = BedTool.from_dataframe(bed1_df[bed1_cols].sort_values(bed1_cols))
+    bed_2 = BedTool.from_dataframe(bed2_df[bed2_cols].sort_values(bed2_cols))
+    return BedTool.to_dataframe(bed_1.intersect(bed_2, wa=True, wb=True))
+    
+def CpGs_to_features(met, ref, metcols, refcols, loffset=0, roffset=1):
+    # met: datatable
+    # ref: pandas
+    # metcols: [chrom, pos, other columns that are not samples]
+    # refcols: [chrom, start, end, name]
+    cpgpos = met[:, [metcols[0], metcols[1]]].to_pandas()
+    cpgpos['start'] = cpgpos[metcols[1]]+loffset
+    cpgpos['end'] = cpgpos[metcols[1]]+roffset
+
+    cpgsoi = find_overlapping_regions(ref, cpgpos, refcols, [metcols[0],'start','end'])
+    cpgsoi.index = cpgsoi['score'].astype(str)+'@'+(cpgsoi['strand']-loffset).astype(str)
+
+    cpgpos['name'] = cpgpos[metcols[0]].astype(str)+'@'+cpgpos[metcols[1]].astype(str)
+    cpgpos['name'] = cpgpos['name'].map(cpgsoi['name'].to_dict())
+
+    met_features = met.copy()
+    del met_features[: , metcols]
+    met_features['name'] = dt.Frame(cpgpos['name'])
+    met_features = met_features[:, dt.mean(f[:]), by('name')]
+
+    return met_features
